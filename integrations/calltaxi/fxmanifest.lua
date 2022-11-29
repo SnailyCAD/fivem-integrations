1fdc0a0c56040e2d697bb23db52f3cfa25664d28
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-11-29 10:00:25 +0100

    chore(release): 1.0.0

--- integrations/calltaxi/fxmanifest.lua
+++ integrations/calltaxi/fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Dev-CasperTheGhost"
 description "/calltaxi command that connects to SnailyCADv4"
-version "0.0.5"
+version "1.0.0"
 
 server_scripts {"server/server.js"}
 client_scripts {"client/client.js"}
