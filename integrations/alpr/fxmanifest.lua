8206b8ec2ca335c2d1c117beb78c5577ea10954a
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2024-06-12 19:18:19 +0200

    chore(release): 0.14.0

--- integrations/alpr/fxmanifest.lua
+++ integrations/alpr/fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Casper Iversen"
 description "ALPR integration with support for Wraith ARS 2X"
-version "0.13.1"
+version "0.14.0"
 
 server_scripts {"server/server.js"}
 client_scripts {"client/client.js"}
--- integrations/call911/fxmanifest.lua
+++ integrations/call911/fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Casper Iversen"
 description "/call911 command that connects to SnailyCADv4"
-version "0.13.1"
+version "0.14.0"
 
 server_scripts {"server/server.js"}
 client_scripts {"client/client.js"}
