51335f6abb813765fc80f421ced39e3cb572fc89
voxty <135042947+voxty@users.noreply.github.com>
2024-10-18 23:46:39 -0400

    Update client.ts

--- integrations/call911/client/client.ts
+++ integrations/call911/client/client.ts
@@ -1,7 +1,7 @@
-import { createNotification } from "~/utils/notification";
-import { TextureTypes } from "~/types/texture-types";
-import { IconTypes } from "~/types/icon-types";
 import { EventData, Events, SnCommands } from "~/types/events";
+import { getPostal } from "~/utils/postal/getPostal";
+
+const usePostal = GetConvar("snailycad_use_postal", "false") === "true";
 
 emit("chat:addSuggestion", `/${SnCommands.Call911}`, "Contact the emergency services.", [
   { name: "description", help: "The description of the call" },
@@ -25,20 +25,103 @@ onNet(Events.Call911ToClient, ({ source, name, description }: EventData) => {
   });
 });
 
-onNet(Events.Call911ToClientResponse, (state: "failed" | "success") => {
+onNet(Events.DispatcherinChannel, () => {
+    TriggerEvent('chat:addMessage', {
+      color: [255, 255, 255],
+      multiline: true,
+      args: ["^1 [Emergency Services]", "^7 There is an active dispatcher in channel please join the 911 Call Center on Teamspeak."]
+  })
+});
+
+onNet(Events.Call911ToClientResponse, ((state: "failed" | "success") => {
   if (state === "success") {
-    createNotification({
-      picture: TextureTypes.CHAR_CALL911,
-      icon: IconTypes.ChatBox,
-      message: "Your call has been reported to the emergency services",
-      title: "Emergency Services",
-    });
+    TriggerEvent('chat:addMessage', {
+      color: [255, 255, 255],
+      multiline: true,
+      args: ["^1 [Emergency Services]", "^7 Your 911 has been reported to any available emergency services!"]
+  });
   } else {
-    createNotification({
-      picture: TextureTypes.CHAR_CALL911,
-      icon: IconTypes.ChatBox,
-      message: "We were unable to process your 911 call at this time.",
-      title: "Failed to report call",
+    TriggerEvent('chat:addMessage', {
+      color: [255, 255, 255],
+      multiline: true,
+      args: ["^1 [Emergency Services]", "^7 We were unable to process your 911 call at this time."]
+  });
+  }
+}));
+
+onNet(Events.Send911ToAll, async ({ name, description, position }: { name: string; description: string } & { position: any }) => {
+
+  const postal = usePostal ? await getPostal(position) : null;
+  
+  const playerPed: number = GetPlayerPed(-1);
+  const [x, y, z]: number[] = GetEntityCoords(playerPed, true);
+  const [lastStreet]: number[] = GetStreetNameAtCoord(x!, y!, z!);
+  const lastStreetName: string = GetStreetNameFromHashKey(lastStreet);
+
+  TriggerEvent('chat:addMessage', {
+    color: [255, 255, 255],
+    multiline: true,
+    args: [
+      "^1 [911]", 
+      "^7" + name + " | Call Description: " + description + " | Postal: " + postal + " | Street: " + lastStreetName
+    ]
+  });
+})
+
+
+onNet(Events.Send911, ({ source, description }: EventData) => {
+  console.log(`911 Call Received from Player ID: ${source}. Description: ${description}`);
+
+  const playerPed: number = GetPlayerPed(-1);
+  const [x, y, z]: number[] = GetEntityCoords(playerPed, true);
+  const [lastStreet]: number[] = GetStreetNameAtCoord(x!, y!, z!);
+  const lastStreetName: string = GetStreetNameFromHashKey(lastStreet);
+  
+  let proximity: number = 500;
+  let foundPlayers: boolean = false;
+
+  while (!foundPlayers && proximity <= 2000) {
+    const playersInProximity: number[] = [];
+    
+    const players: number[] = GetActivePlayers();
+
+    for (const playerId of players) {
+      if (playerId !== source) {
+        const targetPed: number = GetPlayerPed(playerId);
+        const [targetX, targetY, targetZ]: number[] = GetEntityCoords(targetPed, true);
+        
+        const distance: number = Vdist(x!, y!, z!, targetX ?? 0, targetY ?? 0, targetZ ?? 0);
+        
+        if (distance <= proximity) {
+          playersInProximity.push(playerId);
+        }
+      }
+    }
+
+    if (playersInProximity.length > 0) {
+      foundPlayers = true;
+      TriggerEvent('chat:addMessage', playersInProximity, {
+        color: [255, 255, 255],
+        multiline: true,
+        args: [
+          "^1 [Emergency Services]", 
+          "^7 A 911 call has been received | " + description + " | Located At: " + lastStreetName
+        ]
+      });
+    } else {
+      proximity += 100;
+    }
+
+    if (playersInProximity.length >= 4) {
+      foundPlayers = true;
+    }
+  }
+
+  if (!foundPlayers) {
+    TriggerEvent('chat:addMessage', -1, {
+      color: [255, 255, 255],
+      multiline: true,
+      args: ["^1 [Emergency Services]", "^7 A 911 call has been received | " + description + " | Located At: " + lastStreetName]
     });
   }
 });
