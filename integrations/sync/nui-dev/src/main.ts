acc626493b5ddfc6b79ebdc62e15d57cdb73a233
kristian770 <kristian77010@gmail.com>
2024-12-31 20:07:04 +0100

    fix(sync): :bug: sync now uses the actual resource name
    sna-sync now uses the GetParentResourceName in order to get the name of the resource. Also added error handling that prints out the url that is in the fetch request.

--- integrations/sync/nui-dev/src/main.ts
+++ integrations/sync/nui-dev/src/main.ts
@@ -17,9 +17,7 @@ export interface NuiMessage {
 }
 
 declare global {
-  interface Window {
-    GetCurrentResourceName?(): string;
-  }
+  function GetParentResourceName(): string;
 }
 
 window.addEventListener("message", (event: MessageEvent<NuiMessage>) => {
@@ -95,8 +93,15 @@ export async function fetchNUI(eventName: NuiEvents, data = {}) {
       body: JSON.stringify(data),
     } as const;
 
-    const resourceName = window.GetCurrentResourceName?.() ?? "sna-sync";
-    const response = await fetch(`https://${resourceName}/${eventName}`, options);
+    const resourceName = GetParentResourceName();
+    const url = `https://${resourceName}/${eventName}`;
+    const response = await new Promise((resolve, reject) => {
+      fetch(url, options)
+        .then((value) => resolve(value))
+        .catch((reason) => {
+          reject(new Error(`Failed to fetch url: ("${url}"), Reason: ${reason}`));
+        });
+    });
 
     console.log("[`sna-sync-nui`][outgoing]:", eventName);
 
