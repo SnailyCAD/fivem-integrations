b043a3ea5ffbaa68a058c5b5879c91f6197f098d
casperiv <53900565+casperiv0@users.noreply.github.com>
2025-01-03 11:30:59 +0100

    chore(release): 0.14.3

--- integrations/calltow/fxmanifest.lua
+++ integrations/calltow/fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Casper Iversen"
 description "/calltow command that connects to SnailyCADv4"
-version "0.14.2"
+version "0.14.3"
 
 server_scripts {"server/server.js"}
 client_scripts {"client/client.js"}
